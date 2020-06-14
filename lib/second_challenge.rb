1498ef1d939b6110a86316aded6c583129404da4
Harley Hanna <harleyhannawrit@gmail.com>
2020-06-14 18:07:58 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -8,9 +8,9 @@ def second_challenge
       
     },
     :capulet => {
-      :patriarch => {},
-      :matriarch => {},
-      :heroine => {},
+      :patriarch => {:name => "Lord Capulet", :age => "50"},
+      :matriarch => {:name => "Lady Capulet", :age => "51"},
+      :heroine => {:name => "Juliet", :age => "15", :status => "alive"},
       :heroine_friends => []
       }
   }
